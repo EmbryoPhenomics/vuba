df8e68878904daa82b11ae1b98f472038466bc36
zibbini <ziadibbini@gmail.com>
2021-01-21 11:25:17 +0000

    Add flag for reading in images with a bit depth > 8.

--- vuba/imio.py
+++ vuba/imio.py
@@ -387,7 +387,7 @@ class Video:
             self.video.set(1, index)
             success, frame = self.video.read()
         else:
-            frame = cv2.imread(self.filenames[index])
+            frame = cv2.imread(self.filenames[index], cv2.IMREAD_ANYDEPTH)
 
         if grayscale:
             frame = ops.gray(frame)
@@ -428,7 +428,7 @@ class Video:
                     yield frame
         else:
             for fn in self.filenames[slice(start, stop, step)]:
-                frame = cv2.imread(fn)
+                frame = cv2.imread(fn, cv2.IMREAD_ANYDEPTH)
                 if grayscale:
                     frame = ops.gray(frame)
                 yield frame
